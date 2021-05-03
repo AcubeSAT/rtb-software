c48c02c767ffb31ae87d0e754ada623386165389
kongr45gpen <electrovesta@gmail.com>
2021-05-03 21:30:28 +0300

    Update sampling time?

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -405,7 +405,7 @@ static void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_15;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  sConfig.SamplingTime = ADC_SAMPLETIME_810CYCLES_5;
   sConfig.SingleDiff = ADC_SINGLE_ENDED;
   sConfig.OffsetNumber = ADC_OFFSET_NONE;
   sConfig.Offset = 0;
